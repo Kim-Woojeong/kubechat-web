bbe6bf30388e708a5a58bb990bcb79c23757f8e5
woojeong <kyj990307@naver.com>
2022-01-21 21:30:50 +0900

    [Add] left-sidebar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,40 @@
-import '../public/css/Home.css';
+import "../public/css/Home.css";
 import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      {/* App-header */}
       <header className="App-header">
-                <ul className="menu">
-                    <p className="logo">Logo</p>
-                    <ul className="homemenu">
-                    <li>프로젝트 소개</li>
-                    <li>목표</li>
-                    <li>Contact</li>
-                    </ul>
-                    <li className="usermenu">
-                        <Link to="/login"  className="login">login</Link>
-                    </li>
-                    </ul>
-                </header>
+        <ul className="menu">
+          <p className="logo">Logo</p>
+          <ul className="homemenu">
+            <li>프로젝트 소개</li>
+            <li>목표</li>
+            <li>Contact</li>
+          </ul>
+          <li className="usermenu">
+            <Link to="/login" className="login">
+              login
+            </Link>
+          </li>
+        </ul>
+      </header>
+      {/* End App-header */}
+      {/* main-container */}
       <div className="main-container">
         Home
+        {/* left-sidebar */}
+        <div className="left-sidebar">left-sidebar</div>
+        {/* End left-sidebar */}
+        {/* page-wrapper */}
+        <div className="page-wrapper"></div>
+        {/* page-wrapper */}
       </div>
-      <footer className="footer">
-        
-      </footer>
+      {/* End main-container */}
+      {/* footer */}
+      <footer className="footer"></footer>
+      {/* End footer */}
     </div>
   );
 }
